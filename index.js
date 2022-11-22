a3a5e0cd0058d57a9aa12fb2228687fbcf1d6167
Petrochenk0 <96386665+Petrochenk0@users.noreply.github.com>
2022-11-22 19:53:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 tasks = [];
 let promptTask;
-
+console.time("time1")
 do{
 	promptTask = prompt("Введите задачу: ");
-
+	console.info(`promptTask: ${promptTask}`)
 	if(!promptTask) break;
 
 	tasks.push(promptTask);
 
 	let message = "";
-
+	
 	for(let i = 0;i<tasks.lenght;i++){
 		message = message + `${i + 1}. ${tasks[i]}\n`
 	}
-
+	
 	alert("Ваш список задач:\n " + message);
 
 }while(true);
-
+console.time("time1");
 alert("Программа завершила выполнение!");
+
+console.table(tasks);
